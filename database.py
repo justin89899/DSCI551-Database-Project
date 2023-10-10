6a16eab4045608a21da7359fccb5996444b360c1
justin89899 <justice89899@gmail.com>
2023-10-10 14:04:24 -0700

    edit interface.py and database.py

--- database.py
+++ database.py
@@ -1,1 +1,42 @@
 ## this is our main program
+class Database:
+    def __init__(self):
+        self.tables = {}
+
+    # def create_table(self, table_name, columns):
+    #     if table_name in self.tables:
+    #         raise ValueError(f"Table {table_name} already exists.")
+    #     self.tables[table_name] = {"columns": columns, "data": []}
+
+    # def insert(self, table_name, values):
+    #     if table_name not in self.tables:
+    #         raise ValueError(f"Table {table_name} doesn't exist.")
+    #     if len(values) != len(self.tables[table_name]["columns"]):
+    #         raise ValueError("Number of columns doesn't match.")
+    #     self.tables[table_name]["data"].append(values)
+
+    def get(self, table_name, columns, conditions=None):
+        print(f"output columns {columns} from table {table_name} with conditions {conditions}")
+        # if table_name not in self.tables:
+        #     raise ValueError(f"Table {table_name} doesn't exist.")
+        # data = self.tables[table_name]["data"]
+
+        # if not conditions:
+        #     results = data
+        # else:
+        #     results = []
+        #     for row in data:
+        #         if self._evaluate_conditions(row, conditions):
+        #             results.append(row)
+
+        # if not columns or columns == ["*"]:
+        #     return results
+        # return [self._filter_columns(row, columns) for row in results]
+
+    # def _evaluate_conditions(self, row, conditions):
+    #     # Implement your condition evaluation logic here
+    #     # For simplicity, this template assumes that all conditions are met
+    #     return True
+
+    # def _filter_columns(self, row, columns):
+    #     return {col: row[col] for col in columns}
---
+++ interface.py
@@ -0,0 +1,51 @@
+
+from database import Database
+
+def main():
+    db = Database()
+
+    while True:
+        query = input("Enter a query (or 'exit' to quit): ").strip().replace(",","")
+        if query.lower() == 'exit':
+            break
+
+        try:
+            if query.startswith("GET"):
+                # Parse and execute GET statement
+                # Example: GET id, name FROM users
+                parts = query.split(" ")
+                from_index = parts.index("FROM")
+                table_name = parts[from_index+1]
+                columns = parts[1:from_index]
+                conditions = None
+                if "WHERE" in parts:
+                    where_index = parts.index("WHERE")
+                    conditions = parts[where_index + 1:]
+
+                db.get(table_name, columns, conditions)
+
+                #for row in result:
+                    #print(row)
+            # elif query.startswith("CREATE TABLE"):
+            #     # Parse and execute CREATE TABLE statement
+            #     # Example: CREATE TABLE users (id INT, name TEXT)
+            #     parts = query.split(" ")
+            #     table_name = parts[2]
+            #     columns = parts[4].split(",")
+            #     db.create_table(table_name, [col.strip() for col in columns])
+
+            # elif query.startswith("INSERT INTO"):
+            #     # Parse and execute INSERT INTO statement
+            #     # Example: INSERT INTO users VALUES (1, 'Alice')
+            #     parts = query.split(" ")
+            #     table_name = parts[2]
+            #     values = eval("[" + query.split("VALUES")[1].strip() + "]")
+            #     db.insert(table_name, values)
+            else:
+                print("Invalid query.")
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    main()
