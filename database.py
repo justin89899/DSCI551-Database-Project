fa0f7eb04f5f0bece4c121b96810387ad7afd17a
justin89899 <justice89899@gmail.com>
2023-11-06 15:38:40 -0800

    add database options

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
 ## this is our main program
-class Database:
+class SQL_Database:
     def __init__(self):
         self.tables = {}
-
-    # def create_table(self, table_name, columns):
-    #     if table_name in self.tables:
-    #         raise ValueError(f"Table {table_name} already exists.")
-    #     self.tables[table_name] = {"columns": columns, "data": []}
 
     def insert(self, table_info, values):
         
@@ -28,37 +23,44 @@ class Database:
         if not condition:
             for ele in items:
                 self.tables[table_name][ele] = None
-        #else:
-            #evaluate_conditions()
 
     def update(self, table_name, values, condition):
         print(f"update values {values} from table {table_name} on condition {condition}")
         if table_name not in self.tables:
             print(f"Table {table_name} doesn't exist.")
-            #evaluate_conditions()
 
     def get(self, table_name, columns, connect_table, on_condition, conditions=None, grouping=None, ordering=None, order_by=None):
         print(f"output columns {columns} from table {table_name} connect with table {connect_table} on {on_condition} with conditions {conditions} gather by {grouping} order by {order_by} in {ordering} order")
-        # if table_name not in self.tables:
-        #     raise ValueError(f"Table {table_name} doesn't exist.")
-        # data = self.tables[table_name]["data"]
 
-        # if not conditions:
-        #     results = data
-        # else:
-        #     results = []
-        #     for row in data:
-        #         if self._evaluate_conditions(row, conditions):
-        #             results.append(row)
+class noSQL_Database:
+    def __init__(self):
+        self.tables = {}
 
-        # if not columns or columns == ["*"]:
-        #     return results
-        # return [self._filter_columns(row, columns) for row in results]
+    def insert(self, table_info, values):
+        
+        table_name = table_info[0]
+        table_columns = " ".join(table_info[1:]).replace("(","").replace(")","").replace(",","")
+        table_columns = table_columns.split(" ")
+        print(f"Put values {values} to table {table_name} on columns {table_columns}")
+        if table_name not in self.tables:
+            print(f"Table {table_name} doesn't exist.")
+        if len(values) != len(self.tables[table_name]):
+            print("Number of columns doesn't match.")
+        for i in range(self.tables[table_name]):
+            self.tables[table_name][i].append(values[i])
 
-    # def _evaluate_conditions(self, row, conditions):
-    #     # Implement your condition evaluation logic here
-    #     # For simplicity, this template assumes that all conditions are met
-    #     return True
+    def delete(self, table_name, items, condition):
+        print(f"delete items {items} from table {table_name} on condition {condition}")
+        if table_name not in self.tables:
+            print(f"Table {table_name} doesn't exist.")
+        if not condition:
+            for ele in items:
+                self.tables[table_name][ele] = None
 
-    # def _filter_columns(self, row, columns):
-    #     return {col: row[col] for col in columns}
+    def update(self, table_name, values, condition):
+        print(f"update values {values} from table {table_name} on condition {condition}")
+        if table_name not in self.tables:
+            print(f"Table {table_name} doesn't exist.")
+
+    def get(self, table_name, columns, connect_table, on_condition, conditions=None, grouping=None, ordering=None, order_by=None):
+        print(f"output columns {columns} from table {table_name} connect with table {connect_table} on {on_condition} with conditions {conditions} gather by {grouping} order by {order_by} in {ordering} order")
--- interface.py
+++ interface.py
@@ -1,144 +1,140 @@
 
-from database import Database
+from database import *
 
 def main():
-    db = Database()
-    key_words = ["GET","FROM","CONNECT","WHEN","GATHER_BY","ASCEND_BY","DESCEND_BY","PUT","DROP","CHANGE"]
     while True:
-        query = input("Enter a query (or 'exit' to quit): ").strip()
-        if query.lower() == 'exit':
-            break
-        # get key words index
-        parts = query.split(" ")
-        key_words_index = []
-        for i,k in enumerate(parts):
-            if k in key_words:
-                key_words_index.append(i)
-        key_words_index.append("end")
-        try:
-            if query.startswith("GET"):
-                # Parse and execute GET statement
-                # Example: GET id, name FROM users
-                from_index = parts.index("FROM")
+        # choose database
+        databases = ["SQL", "noSQL"]
+        db_to_use = input("Choose a database to use (SQL, noSQL) or exit:")
+        if db_to_use not in databases:
+            if db_to_use == 'exit':
+                break
+            print("not a existing database")
+            continue
+        if db_to_use=="SQL":
+            db = SQL_Database()
+        else:
+            db = noSQL_Database()
 
-                # parse all the columns
-                columns = [i.replace(",","") for i in parts[1:from_index]]
+        key_words = ["GET","FROM","CONNECT","WHEN","GATHER_BY","ASCEND_BY","DESCEND_BY","PUT","DROP","CHANGE"]
+        while True:
+            
+            query = input("Enter a query (or 'exit' to quit): ").strip()
+            if query.lower() == 'exit':
+                break
+            # get key words index
+            parts = query.split(" ")
+            key_words_index = []
+            for i,k in enumerate(parts):
+                if k in key_words:
+                    key_words_index.append(i)
+            key_words_index.append("end")
+            try:
+                if query.startswith("GET"):
+                    # Parse and execute GET statement
+                    # Example: GET id, name FROM users
+                    from_index = parts.index("FROM")
 
-                # parse all the tables
-                i = 0
-                tables = []
-                while parts[from_index+1+i]:
+                    # parse all the columns
+                    columns = [i.replace(",","") for i in parts[1:from_index]]
 
-                    tables.append(parts[from_index+1+i])
-                    if parts[from_index+1+i].endswith(","):
-                        i = i + 1
-                    else:
-                        break
-                
-                # parse join (CONNECT)
-                connect_table = None
-                if "CONNECT" in parts:
-                    connect_index = parts.index("CONNECT")
-                    connect_table = parts[connect_index + 1]
-                    #on_condition = parts[connect_index + 3:connect_index + 6]
-                    if key_words_index[key_words_index.index(connect_index)+1] == "end":
-                        on_condition = parts[connect_index + 3:]
-                    else:
-                        on_condition = parts[connect_index + 3:key_words_index[key_words_index.index(connect_index)+1]]
-                # parse condition (WHEN)
-                conditions = None
-                if "WHEN" in parts:
-                    when_index = parts.index("WHEN")
-                    if key_words_index[key_words_index.index(when_index)+1] == "end":
-                        conditions = parts[when_index + 1:]
-                    else:
-                        conditions = parts[when_index + 1:key_words_index[key_words_index.index(when_index)+1]]
-                
-                # parse grouping (GATHER BY)    
-                grouping = None
-                if "GATHER_BY" in parts:
-                    group_index = parts.index("GATHER_BY")
-                    if key_words_index[key_words_index.index(group_index)+1] == "end":
-                        grouping = parts[group_index + 1:]
-                    else:
-                        grouping = parts[group_index + 1:key_words_index[key_words_index.index(group_index)+1]]
+                    # parse all the tables
+                    i = 0
+                    tables = []
+                    while parts[from_index+1+i]:
 
-                # parse ordering (ASCEND_BY/DESCEND_BY)    
-                ordering = None
-                order_by = None
-                if "ASCEND_BY" in parts:
-                    ordering_index = parts.index("ASCEND_BY")
-                    ordering = "ASC"
-                    if key_words_index[key_words_index.index(ordering_index)+1] == "end":
-                        order_by = parts[ordering_index + 1:]
-                    else:
-                        order_by = parts[ordering_index + 1:key_words_index[key_words_index.index(ordering_index)+1]]
-                elif "DESCEND_BY" in parts:
-                    ordering_index = parts.index("DESCEND_BY")
-                    ordering = "DSC"
-                    if key_words_index[key_words_index.index(ordering_index)+1] == "end":
-                        order_by = parts[ordering_index + 1:]
-                    else:
-                        order_by = parts[ordering_index + 1:key_words_index[key_words_index.index(ordering_index)+1]]
+                        tables.append(parts[from_index+1+i])
+                        if parts[from_index+1+i].endswith(","):
+                            i = i + 1
+                        else:
+                            break
                     
-                db.get(tables, columns, connect_table, on_condition, conditions, grouping, ordering, order_by)
+                    # parse join (CONNECT)
+                    connect_table = None
+                    if "CONNECT" in parts:
+                        connect_index = parts.index("CONNECT")
+                        connect_table = parts[connect_index + 1]
+                        #on_condition = parts[connect_index + 3:connect_index + 6]
+                        if key_words_index[key_words_index.index(connect_index)+1] == "end":
+                            on_condition = parts[connect_index + 3:]
+                        else:
+                            on_condition = parts[connect_index + 3:key_words_index[key_words_index.index(connect_index)+1]]
+                    # parse condition (WHEN)
+                    conditions = None
+                    if "WHEN" in parts:
+                        when_index = parts.index("WHEN")
+                        if key_words_index[key_words_index.index(when_index)+1] == "end":
+                            conditions = parts[when_index + 1:]
+                        else:
+                            conditions = parts[when_index + 1:key_words_index[key_words_index.index(when_index)+1]]
+                    
+                    # parse grouping (GATHER BY)    
+                    grouping = None
+                    if "GATHER_BY" in parts:
+                        group_index = parts.index("GATHER_BY")
+                        if key_words_index[key_words_index.index(group_index)+1] == "end":
+                            grouping = parts[group_index + 1:]
+                        else:
+                            grouping = parts[group_index + 1:key_words_index[key_words_index.index(group_index)+1]]
 
-                #for row in result:
-                    #print(row)
-            # elif query.startswith("CREATE TABLE"):
-            #     # Parse and execute CREATE TABLE statement
-            #     # Example: CREATE TABLE users (id INT, name TEXT)
-            #     parts = query.split(" ")
-            #     table_name = parts[2]
-            #     columns = parts[4].split(",")
-            #     db.create_table(table_name, [col.strip() for col in columns])
+                    # parse ordering (ASCEND_BY/DESCEND_BY)    
+                    ordering = None
+                    order_by = None
+                    if "ASCEND_BY" in parts:
+                        ordering_index = parts.index("ASCEND_BY")
+                        ordering = "ASC"
+                        if key_words_index[key_words_index.index(ordering_index)+1] == "end":
+                            order_by = parts[ordering_index + 1:]
+                        else:
+                            order_by = parts[ordering_index + 1:key_words_index[key_words_index.index(ordering_index)+1]]
+                    elif "DESCEND_BY" in parts:
+                        ordering_index = parts.index("DESCEND_BY")
+                        ordering = "DSC"
+                        if key_words_index[key_words_index.index(ordering_index)+1] == "end":
+                            order_by = parts[ordering_index + 1:]
+                        else:
+                            order_by = parts[ordering_index + 1:key_words_index[key_words_index.index(ordering_index)+1]]
+                        
+                    db.get(tables, columns, connect_table, on_condition, conditions, grouping, ordering, order_by)
+                
+                elif query.startswith("PUT"):
+                    # Parse and execute PUT IN statement
+                    # Example: PUT 1, 'Alice' IN users
+                    IN_index = parts.index("IN")
+                    table_name = parts[IN_index+1:]
+                    # parse all the values
+                    values = [i.replace(",", "") for i in parts[1:IN_index]]
+                    db.insert(table_name, values)
 
-            # elif query.startswith("INSERT INTO"):
-            #     # Parse and execute INSERT INTO statement
-            #     # Example: INSERT INTO users VALUES (1, 'Alice')
-            #     parts = query.split(" ")
-            #     table_name = parts[2]
-            #     values = eval("[" + query.split("VALUES")[1].strip() + "]")
-            #     db.insert(table_name, values)
-            
-            elif query.startswith("PUT"):
-                 # Parse and execute PUT IN statement
-                 # Example: PUT 1, 'Alice' IN users
-                 IN_index = parts.index("IN")
-                 table_name = parts[IN_index+1:]
-                 # parse all the values
-                 values = [i.replace(",", "") for i in parts[1:IN_index]]
-                 db.insert(table_name, values)
+                elif query.startswith("DROP"):
+                    # Parse and execute DROP sth FROM table WHEN statement
+                    # Example: DROP name FROM table WHEN id = 100
+                    FROM_index = parts.index("FROM")
+                    conditions = None
+                    if "WHEN" in parts:
+                        WHEN_index = parts.index("WHEN")
+                        table_name = parts[FROM_index+1:WHEN_index]
+                        conditions = parts[WHEN_index+1:]
+                    # Parse the items
+                    items = [i.replace(",", "") for i in parts[1:FROM_index]]
 
-            elif query.startswith("DROP"):
-                 # Parse and execute DROP sth FROM table WHEN statement
-                 # Example: DROP name FROM table WHEN id = 100
-                 FROM_index = parts.index("FROM")
-                 conditions = None
-                 if "WHEN" in parts:
+                    db.delete(table_name, items, conditions)
+
+                elif query.startswith("CHANGE"):
+                    # Parse and execute CHANGE table WITH values WHEN
+                    # Example: CHANGE users WITH name = steve
+                    WITH_index = parts.index("WITH")
+                    table_name = parts[1]
                     WHEN_index = parts.index("WHEN")
-                    table_name = parts[FROM_index+1:WHEN_index]
+                    values = [i.replace(",", "") for i in parts[WITH_index + 1: WHEN_index]]
                     conditions = parts[WHEN_index+1:]
-                 # Parse the items
-                 items = [i.replace(",", "") for i in parts[1:FROM_index]]
+                    db.update(table_name, values, conditions)
+                
+                else:
+                    print("Invalid query.")
 
-                 db.delete(table_name, items, conditions)
-
-            elif query.startswith("CHANGE"):
-                 # Parse and execute CHANGE table WITH values WHEN
-                 # Example: CHANGE users WITH name = steve
-                 WITH_index = parts.index("WITH")
-                 table_name = parts[1]
-                 WHEN_index = parts.index("WHEN")
-                 values = [i.replace(",", "") for i in parts[WITH_index + 1: WHEN_index]]
-                 conditions = parts[WHEN_index+1:]
-                 db.update(table_name, values, conditions)
-            
-            else:
-                print("Invalid query.")
-
-        except Exception as e:
-            print(f"Error: {str(e)}")
+            except Exception as e:
+                print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
     main()
